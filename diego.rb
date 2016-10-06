91d6a9764f3c525333e61a9dd96327fa75ba01b0
Nicolò Vescera <slayercandy@icloud.com>
2016-10-06 19:44:53 +0200

    Aggiunta funzione gratuito o gratuìto

--- diego.rb
+++ diego.rb
@@ -31,5 +31,15 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
 			end
 		end
 
+		#gratuito o gratuito?
+		if message.text.to_s.downcase.include?("gratuito") || message.text.to_s.downcase.include?("gratuìto")
+			case Random.rand(0...11)
+				when 0..9
+					bot.api.send_message(chat_id: message.chat.id, text: "Beh ovvio gratìto .. Ah no gratuì.. gra.. gratuìt... gratuìtoh... No basta mi arrendo")
+				when 10
+					bot.api.send_message(chat_id: message.chat.id, text: "Beh è gratuìto .. ah eh grat.. gratuito. SIIII SIIII GRATUITO !!!")
+			end
+		end
+
 	end
 end
